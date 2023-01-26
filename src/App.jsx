9bd07e1d8bbf07707934e93194fe64e4ab7f688f
enoquetembe <enoquetembetembe10@gmail.com>
2023-01-26 19:51:48 -0300

    getting started

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+export function App() {
+ 
+  return (
+   <div className='w-screen h-screen bg-zinc-900'>
+    <h1 className='text-6xl font-bold'>Helloooo</h1>
+   </div>
+  )
+}
+
